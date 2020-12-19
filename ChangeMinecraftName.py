3cdaec24713773da20406ce39325507eb319f426
Timothy Margono <23224779+tmargono@users.noreply.github.com>
2020-12-18 23:39:32 -0800

    Add time delay

--- ChangeMinecraftName.py
+++ ChangeMinecraftName.py
@@ -1,8 +1,19 @@
-import urllib3
+import urllib3, time
 
 def lambda_handler(event, context):
     https = urllib3.PoolManager()
     wantedName = event.get(wantedName)
     token = event.get(token)
+    delay = event.get(delay)
+    
+    #Delay for specified seconds because Cloudwatch rule smallest increment of time is minutes
+    time.sleep(delay)
+    
+    #API Request, change name
     r = https.request('PUT', f'https://api.minecraftservices.com/minecraft/profile/name/{wantedName}', fields={'Authorization': f'Bearer {token}'})
+    
+    #Print Status Code 
     print(r.status)
+    
+    #Print Result
+    print(r.data.decode('utf-8'))
